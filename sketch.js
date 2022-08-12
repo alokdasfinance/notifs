d498767abac6bca78cb7ce46e697679799d0388c
alokdasfinance <110708048+alokdasfinance@users.noreply.github.com>
2022-08-12 17:39:31 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,18 +1,7 @@
-
 function setup() {
-  var wshShell = new ActiveXObject("WScript.Shell");
-  submit = document.getElementById('newbutton');
-  username = document.getElementById('username').value;
-  password = document.getElementById('password').value;
+  // put setup code here
 }
 
 function draw() {
-
-}
-
-function verify() {
-  if (username == "5" && password == "5") {
-    wshShell.Run("C:\Users\wonde\Algo\auto.bat");
-  }
-
+  // put drawing code here
 }
